a04452544f45408ae752e9458b1b74e397946f48
Kevin Falting <kevinfalting@gmail.com>
2016-06-05 10:39:12 -0500

    add codepen lion

---
+++ js/isitriverfest.js
@@ -0,0 +1,14 @@
+var startDate = 3;
+var endDate = 11;
+
+var today = new Date();
+var dd = today.getDate();
+var mm = today.getMonth()+1; //January is 0!
+
+if (mm == 6) {
+  if (dd >= startDate && dd <= endDate) {
+    document.write("YES!")
+  }
+} else {
+  document.write("no.")
+}
