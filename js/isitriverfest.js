7d11a740b7c431d0ec5d18317ce89cc8f18a4068
Kevin Falting <kevinfalting@gmail.com>
2016-06-14 14:44:07 -0600

    Fix issue where the else block of isitriverfest.js was not being executed outside of riverfest dates.

--- js/isitriverfest.js
+++ js/isitriverfest.js
@@ -5,10 +5,8 @@ var today = new Date();
 var dd = today.getDate();
 var mm = today.getMonth()+1; //January is 0!
 
-if (mm == 6) {
-  if (dd >= startDate && dd <= endDate) {
-    document.write("YES!")
-  }
+if (mm == 6 && dd >= startDate && dd <= endDate) {
+  document.write("YES!")
 } else {
   document.write("no.")
 }
